20b560247a7fd8abc317c1f45c140eca656fb520
Daniel Grbac Bravo <danielgrbacbravo@gmail.com>
2024-11-12 21:09:40 +0100

    feat: implemented validate_and_format_schema resolves #5 #6 #1 #2 #7
    Co-authored-by: Dalia-Lorena Dinu <dalialorenadinu247@gmail.com>

--- src/validator.py
+++ src/validator.py
@@ -1,22 +1,15 @@
-#TODO: Implement the format validator
+# format validator
 
 import pandas as pd
 from datetime import datetime
 
-#TODO: Implement the schema validator
+# Implement the schema validator
 def validate_schema(data: pd.DataFrame, schema: pd.DataFrame) -> bool:
   # return True if each column in the data is present in the schema
   schema_question_text = schema['questionText']
   return all(column in schema_question_text for column in data.columns)
 
-
-# TODO: Implement the age validator
-def validate_age(data: pd.DataFrame) -> bool:
-  # return True if the 'Age' column is present in the data
-  return True
-
-
-#TODO: Implement the date time format determiner
+# Implement the date time format determiner
 def determine_datetime_format(date_str: str) -> str:
     formats = ['%d-%m-%Y', '%m-%d-%Y', '%Y-%m-%d', '%d/%m/%Y', '%m/%d/%Y', '%Y/%m/%d']
     for fmt in formats:
