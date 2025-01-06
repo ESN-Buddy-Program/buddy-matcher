297ea64d31597fd50abf6f9f2331bd2df1e6f62d
Dalia-Lorena Dinu <dalialorenadinu247@gmail.com>
2025-01-06 19:35:13 +0100

    function for personality distance

--- src/distance_calculator.py
+++ src/distance_calculator.py
@@ -64,3 +64,98 @@ def calculate_age_distance(
     age_difference = abs(int(local_age) - int(incoming_age))
 
     return sigmoid(age_difference/age_tolerance)
+
+def is_same_university(incoming_student: pd.Series, local_student: pd.Series) -> float:
+    """
+    Check if the university or study field matches between an incoming and a local student.
+
+    Parameters:
+        incoming_student (pd.Series): Details of the incoming student.
+        local_student (pd.Series): Details of the local student.
+
+    Returns:
+        float: 0.0 if there's a match in university and study field, 0.5 for a match in
+       university and 1.0 otherwise.
+    """
+    if incoming_student['University'] == local_student['University']:
+        if incoming_student ['Faculty'] == local_student['Faculty']:
+            return 0.0
+        else:
+            return 0.5
+    else:
+        return 1.0
+
+def similar_personalities(incoming_student: pd.Series, local_student: pd.Series) -> float:
+    """
+    Check if the personalities matches between an incoming and a local student.
+
+    Parameters:
+        incoming_student (pd.Series): Details of the incoming student.
+        local_student (pd.Series): Details of the local student.
+
+    Returns:
+        float: 0.0 if there's a match in personalities, 1.0 otherwise.
+    """
+    personality_match: float = 0.0
+
+    if ((incoming_student['openess'] == local_student['openess']) or
+        (incoming_student['openess'] < local_student['openess'] and local_student['openess'] - incoming_student['openess'] <= 2.0) or
+        (incoming_student['openess'] > local_student['openess'] and incoming_student['openess'] - local_student['openess'] <= 2.0)):
+        personality_match += 0.0
+    else:
+        personality_match += 1.0
+
+    if ((incoming_student['conscientiousness'] == local_student['conscientiousness']) or
+        (incoming_student['conscientiousness'] < local_student['conscientiousness'] and local_student['conscientiousness'] - incoming_student['conscientiousness'] <= 2.0) or
+        (incoming_student['conscientiousness'] > local_student['conscientiousness'] and incoming_student['conscientiousness'] - local_student['conscientiousness'] <= 2.0)):
+        personality_match += 0.0
+    else:
+        personality_match += 1.0
+
+    if ((incoming_student['extraversion'] == local_student['extraversion']) or
+        (incoming_student['extraversion'] < local_student['extraversion'] and local_student['extraversion'] - incoming_student['extraversion'] <= 2.0) or
+        (incoming_student['extraversion'] > local_student['extraversion'] and incoming_student['extraversion'] - local_student['extraversion'] <= 2.0)):
+        personality_match += 0.0
+    else:
+        personality_match += 1.0
+
+    if ((incoming_student['agreeableness'] == local_student['agreeableness']) or
+        (incoming_student['agreeableness'] < local_student['agreeableness'] and local_student['agreeableness'] - incoming_student['agreeableness'] <= 2.0) or
+        (incoming_student['agreeableness'] > local_student['agreeableness'] and incoming_student['agreeableness'] - local_student['agreeableness'] <= 2.0)):
+        personality_match += 0.0
+    else:
+        personality_match += 1.0
+
+    if ((incoming_student['neuroticism'] == local_student['neuroticism']) or
+        (incoming_student['neuroticism'] < local_student['neuroticism'] and local_student['neuroticism'] - incoming_student['neuroticism'] <= 2.0) or
+        (incoming_student['neuroticism'] > local_student['neuroticism'] and incoming_student['neuroticism'] - local_student['neuroticism'] <= 2.0)):
+        personality_match += 0.0
+    else:
+        personality_match += 1.0
+
+    if ((incoming_student['selfEfficacy'] == local_student['selfEfficacy']) or
+        (incoming_student['selfEfficacy'] < local_student['selfEfficacy'] and local_student['selfEfficacy'] - incoming_student['selfEfficacy'] <= 2.0) or
+        (incoming_student['selfEfficacy'] > local_student['selfEfficacy'] and incoming_student['selfEfficacy'] - local_student['selfEfficacy'] <= 2.0)):
+        personality_match += 0.0
+    else:
+        personality_match += 1.0
+
+    if personality_match == 5.0:
+        personality_match = 1.0
+
+    return personality_match
+
+def is_expectations(incoming_student: pd.Series, local_student: pd.Series) -> float:
+    """
+    Check if the expectations matches between an incoming and a local student.
+
+    Parameters:
+        incoming_student (pd.Series): Details of the incoming student.
+        local_student (pd.Series): Details of the local student.
+
+    Returns:
+        float: 0.0 if there's a match in expectations, 0.5 for a match in
+       university and 1.0 otherwise.
+    """
+
+    return 0.0
